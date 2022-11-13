7e6d53f1b557e358f44ad484f39a0e712d54048b
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-13 23:15:04 +0100

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,39 @@
 import _ from 'lodash';
-const save = (key, value) => {
-    try {
-        const serializedState = JSON.stringify(value);
-        localStorage.setItem(key, serializedState);
-    } catch (error) {
-        console.error("Set state error: ", error.message);
-    }
-};
-const load = key => {
-    try {
-        const serializedState = localStorage.getItem(key);
-        return serializedState === null ? undefined : JSON.parse(serializedState);
-    } catch (error) {
-        console.error("Get state error: ", error.message);
-    }
-};
-const remove = key => {
-    try {
-        localStorage.removeItem(key);
-    } catch (error) {
-        console.error("Get state error: ", error.message);
-    }
-};
+import * as storage from './storage';
+const mail = document.querySelector("input")
+const message = document.querySelector("textarea")
 const form = document.querySelector(".feedback-form");
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
 
 form.addEventListener("input", _.throttle(saveMessage,1000));
 
-function saveMessage(evt) {
-    evt.preventDefault();
+function saveMessage() {
     const feedback = {
-    email: form.elements.email.value,
-    message:form.elements.message.value,
+        email: mail.value,
+        message: message.value,
     }
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback));
-  
-    
-    console.log(form.elements.email.value);
-    console.log(form.elements.message.value);
-  //form.reset();
+    storage.default.save(LOCALSTORAGE_KEY, feedback);
 }
+const parsedInput = storage.default.load(LOCALSTORAGE_KEY);
 
 window.addEventListener("load", checkStorage)
 
 function checkStorage() {
-    const load = key => {
-  try {
-    const serializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : JSON.parse(serializedState);
-  } catch (error) {
-    console.error("Get state error: ", error.message);
-  }
-};
-    load(LOCALSTORAGE_KEY)
-    if (localStorage.getItem(LOCALSTORAGE_KEY) !== null) {
-        form.elements.email.value = LOCALSTORAGE_KEY.email;
-        form.elements.message.value = LOCALSTORAGE_KEY.message;
-    } 
+    if (parsedInput) {
+        mail.value = parsedInput.email;
+        message.value = parsedInput.message;  
+    } else {
+        mail.value = " ";
+        message.value = " ";
+    }
 }
 
+form.addEventListener("submit", afterSubmit)
+
+function afterSubmit(event) {
+    event.preventDefault();
+    console.log(parsedInput)
+    form.reset();
+    storage.default.remove(LOCALSTORAGE_KEY);
+}
