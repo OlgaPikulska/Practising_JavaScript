e6a0e682eae9ec7d643753807353449b5255c8f3
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-12-03 18:26:17 +0100

    update markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,8 +9,8 @@ const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems
   .map(
-    item =>
-      `<a class="gallery__item" href=${item.original}><img class="gallery__image" src=${item.preview} alt=${item.description}/></a>`
+    (item) =>
+      `<div class="gallery__item"><a class="gallery__link" href=${item.original}><img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/></a></div>`
   )
   .join('');
 gallery.innerHTML = markup;
